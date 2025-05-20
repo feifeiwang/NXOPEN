3c969fcb1936d54830fc4481c1db1f3c0b555037
feifeiwang <w342858341@163.com>
2025-05-21 00:40:37 +0800

    Add files via upload

---
+++ learn_nxopen/L8_block1.cpp
@@ -0,0 +1,225 @@
+//L8_block1
+
+// Mandatory UF Includes
+#include <uf.h>
+#include <uf_object_types.h>
+
+// Internal Includes
+#include <NXOpen/ListingWindow.hxx>
+#include <NXOpen/NXMessageBox.hxx>
+#include <NXOpen/UI.hxx>
+
+// Internal+External Includes
+#include <NXOpen/Annotations.hxx>
+#include <NXOpen/Assemblies_Component.hxx>
+#include <NXOpen/Assemblies_ComponentAssembly.hxx>
+#include <NXOpen/Body.hxx>
+#include <NXOpen/BodyCollection.hxx>
+#include <NXOpen/Face.hxx>
+#include <NXOpen/Line.hxx>
+#include <NXOpen/NXException.hxx>
+#include <NXOpen/NXObject.hxx>
+#include <NXOpen/Part.hxx>
+#include <NXOpen/PartCollection.hxx>
+#include <NXOpen/Session.hxx>
+#include <uf_modl_primitives.h>
+#include <uf_ui.h>
+// Std C++ Includes
+#include <iostream>
+#include <sstream>
+
+using namespace NXOpen;
+using std::string;
+using std::exception;
+using std::stringstream;
+using std::endl;
+using std::cout;
+using std::cerr;
+
+
+//------------------------------------------------------------------------------
+// NXOpen c++ test class 
+//------------------------------------------------------------------------------
+class MyClass
+{
+    // class members
+public:
+    static Session *theSession;
+    static UI *theUI;
+
+    MyClass();
+    ~MyClass();
+
+	void do_it();
+	void print(const NXString &);
+	void print(const string &);
+	void print(const char*);
+
+private:
+	Part *workPart, *displayPart;
+	NXMessageBox *mb;
+	ListingWindow *lw;
+	LogFile *lf;
+};
+
+//------------------------------------------------------------------------------
+// Initialize static variables
+//------------------------------------------------------------------------------
+Session *(MyClass::theSession) = NULL;
+UI *(MyClass::theUI) = NULL;
+
+//------------------------------------------------------------------------------
+// Constructor 
+//------------------------------------------------------------------------------
+MyClass::MyClass()
+{
+
+	// Initialize the NX Open C++ API environment
+	MyClass::theSession = NXOpen::Session::GetSession();
+	MyClass::theUI = UI::GetUI();
+	mb = theUI->NXMessageBox();
+	lw = theSession->ListingWindow();
+	lf = theSession->LogFile();
+
+    workPart = theSession->Parts()->Work();
+	displayPart = theSession->Parts()->Display();
+	
+}
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+MyClass::~MyClass()
+{
+}
+
+//------------------------------------------------------------------------------
+// Print string to listing window or stdout
+//------------------------------------------------------------------------------
+void MyClass::print(const NXString &msg)
+{
+	if(! lw->IsOpen() ) lw->Open();
+	lw->WriteLine(msg);
+}
+void MyClass::print(const string &msg)
+{
+	if(! lw->IsOpen() ) lw->Open();
+	lw->WriteLine(msg);
+}
+void MyClass::print(const char * msg)
+{
+	if(! lw->IsOpen() ) lw->Open();
+	lw->WriteLine(msg);
+}
+
+
+
+
+//------------------------------------------------------------------------------
+// Do something
+//------------------------------------------------------------------------------
+void MyClass::do_it()
+{
+
+	// TODO: add your code here
+	//初始化
+	UF_initialize();
+
+	UF_FEATURE_SIGN sign=UF_NULLSIGN;
+
+	/*
+UF_FEATURE_SIGNS
+Also known as:
+UF_FEATURE_SIGN
+
+Data Members
+UF_NULLSIGN = 0
+create new target solid 创建新目标体
+
+UF_POSITIVE = 1
+add to target solid 求和
+
+UF_NEGATIVE = 2
+subtract from target solid 求差
+
+UF_UNSIGNED = 3
+intersect with target solid 求交
+
+UF_NO_BOOLEAN = 4
+feature has not been booleaned 功能尚未被布尔化
+
+UF_TOP_TARGET = 5
+feature is the "top target" feature, it has no
+"parent" features but does have tool features
+
+UF_UNITE = 6
+feature has been united to target solid
+
+UF_SUBTRACT = 7
+feature has been subtracted from target solid
+
+UF_INTERSECT = 8
+feature has been intersected with target solid
+
+UF_DEFORM_POSITIVE = 9
+feature used to deform the positive side
+of the target sheet
+
+UF_DEFORM_NEGATIVE = 10
+feature used to deform the negative side
+of the target sheet*/
+
+	//创建一个块
+	//块的原点
+	double corner[3] = { 0.0,0.0,0.0 };
+	//块的边长
+	char* edge[3] = { "10.0","20.0","30.0" };
+	tag_t fTAG=NULL_TAG;
+	UF_MODL_create_block1(sign,corner,edge,&fTAG);
+
+	//nx弹出文本框
+	uc1601("创建成功", 1);
+
+	//退出
+	UF_terminate();
+	
+}
+
+//------------------------------------------------------------------------------
+// Entry point(s) for unmanaged internal NXOpen C/C++ programs
+//------------------------------------------------------------------------------
+//  Explicit Execution
+extern "C" DllExport void ufusr( char *parm, int *returnCode, int rlen )
+{
+    try
+    {
+		// Create NXOpen C++ class instance
+		MyClass *theMyClass;
+		theMyClass = new MyClass();
+		theMyClass->do_it();
+		delete theMyClass;
+	}
+    catch (const NXException& e1)
+    {
+		UI::GetUI()->NXMessageBox()->Show("NXException", NXOpen::NXMessageBox::DialogTypeError, e1.Message());
+    }
+	catch (const exception& e2)
+    {
+		UI::GetUI()->NXMessageBox()->Show("Exception", NXOpen::NXMessageBox::DialogTypeError, e2.what());
+    }
+	catch (...)
+    {
+		UI::GetUI()->NXMessageBox()->Show("Exception", NXOpen::NXMessageBox::DialogTypeError, "Unknown Exception.");
+    }
+}
+
+
+//------------------------------------------------------------------------------
+// Unload Handler
+//------------------------------------------------------------------------------
+extern "C" DllExport int ufusr_ask_unload()
+{
+	return (int)NXOpen::Session::LibraryUnloadOptionImmediately;
+}
+
+
