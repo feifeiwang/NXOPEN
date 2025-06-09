825f70a53c6dbba48fbdb89eaec0260987c94cf1
feifeiwang <w342858341@163.com>
2025-06-09 20:26:49 +0800

    Add files via upload

---
+++ learn_nxopen/L29_create_arc.cpp
@@ -0,0 +1,218 @@
+//L29_create_arc
+
+// Mandatory UF Includes
+#include <uf.h>
+#include <uf_object_types.h>
+#include <uf_curve.h>
+#include <uf_csys.h>
+
+
+// Internal Includes
+#include <NXOpen/ListingWindow.hxx>
+#include <NXOpen/NXMessageBox.hxx>
+#include <NXOpen/UI.hxx>
+
+// Internal+External Includes
+#include <NXOpen/Annotations.hxx>
+#include <NXOpen/Assemblies_Component.hxx>
+#include <NXOpen/Assemblies_ComponentAssembly.hxx>
+#include <NXOpen/Body.hxx>
+#include <NXOpen/BodyCollection.hxx>
+#include <NXOpen/Face.hxx>
+#include <NXOpen/Line.hxx>
+#include <NXOpen/NXException.hxx>
+#include <NXOpen/NXObject.hxx>
+#include <NXOpen/Part.hxx>
+#include <NXOpen/PartCollection.hxx>
+#include <NXOpen/Session.hxx>
+
+// Std C++ Includes
+#include <iostream>
+#include <sstream>
+
+using namespace NXOpen;
+using std::string;
+using std::exception;
+using std::stringstream;
+using std::endl;
+using std::cout;
+using std::cerr;
+
+
+//------------------------------------------------------------------------------
+// NXOpen c++ test class 
+//------------------------------------------------------------------------------
+class MyClass
+{
+    // class members
+public:
+    static Session *theSession;
+    static UI *theUI;
+
+    MyClass();
+    ~MyClass();
+
+	void do_it();
+	void print(const NXString &);
+	void print(const string &);
+	void print(const char*);
+
+private:
+	Part *workPart, *displayPart;
+	NXMessageBox *mb;
+	ListingWindow *lw;
+	LogFile *lf;
+};
+
+//------------------------------------------------------------------------------
+// Initialize static variables
+//------------------------------------------------------------------------------
+Session *(MyClass::theSession) = NULL;
+UI *(MyClass::theUI) = NULL;
+
+//------------------------------------------------------------------------------
+// Constructor 
+//------------------------------------------------------------------------------
+MyClass::MyClass()
+{
+
+	// Initialize the NX Open C++ API environment
+	MyClass::theSession = NXOpen::Session::GetSession();
+	MyClass::theUI = UI::GetUI();
+	mb = theUI->NXMessageBox();
+	lw = theSession->ListingWindow();
+	lf = theSession->LogFile();
+
+    workPart = theSession->Parts()->Work();
+	displayPart = theSession->Parts()->Display();
+	
+}
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+MyClass::~MyClass()
+{
+}
+
+//------------------------------------------------------------------------------
+// Print string to listing window or stdout
+//------------------------------------------------------------------------------
+void MyClass::print(const NXString &msg)
+{
+	if(! lw->IsOpen() ) lw->Open();
+	lw->WriteLine(msg);
+}
+void MyClass::print(const string &msg)
+{
+	if(! lw->IsOpen() ) lw->Open();
+	lw->WriteLine(msg);
+}
+void MyClass::print(const char * msg)
+{
+	if(! lw->IsOpen() ) lw->Open();
+	lw->WriteLine(msg);
+}
+
+
+
+
+//------------------------------------------------------------------------------
+// Do something
+//------------------------------------------------------------------------------
+void MyClass::do_it()
+{
+
+	// TODO: add your code here
+	UF_initialize();
+
+
+	//定义参数
+	//结构体
+	UF_CURVE_arc_t arc;
+	arc.start_angle = 0.0;
+	arc.end_angle = 225 * DEGRA;//角度换算弧度
+	arc.arc_center[0] = 0.0;
+	arc.arc_center[1] = 0.0;
+	arc.arc_center[2] = 0.0;
+	arc.radius = 50.0;
+
+	//得到csys的矩阵id
+	tag_t wcsTag = NULL_TAG;
+	UF_CSYS_ask_wcs(&wcsTag);
+	UF_CSYS_ask_matrix_of_object( wcsTag,&arc.matrix_tag);
+
+
+
+	/*
+	Data Members
+
+		matrix_tag
+		tag_t
+		matrix for the CSYS the arc is in
+
+		start_angle
+		double
+		expressed in radians
+
+		end_angle
+		double
+		expressed in radians
+
+		arc_center[3]
+		double
+		center of the arc
+
+		radius
+		double
+		radius of the arc*/
+	tag_t arcTag = NULL_TAG;
+	UF_CURVE_create_arc(&arc,  &arcTag);
+
+
+
+
+	//退出
+	UF_terminate();
+
+
+}
+
+//------------------------------------------------------------------------------
+// Entry point(s) for unmanaged internal NXOpen C/C++ programs
+//------------------------------------------------------------------------------
+//  Explicit Execution
+extern "C" DllExport void ufusr( char *parm, int *returnCode, int rlen )
+{
+    try
+    {
+		// Create NXOpen C++ class instance
+		MyClass *theMyClass;
+		theMyClass = new MyClass();
+		theMyClass->do_it();
+		delete theMyClass;
+	}
+    catch (const NXException& e1)
+    {
+		UI::GetUI()->NXMessageBox()->Show("NXException", NXOpen::NXMessageBox::DialogTypeError, e1.Message());
+    }
+	catch (const exception& e2)
+    {
+		UI::GetUI()->NXMessageBox()->Show("Exception", NXOpen::NXMessageBox::DialogTypeError, e2.what());
+    }
+	catch (...)
+    {
+		UI::GetUI()->NXMessageBox()->Show("Exception", NXOpen::NXMessageBox::DialogTypeError, "Unknown Exception.");
+    }
+}
+
+
+//------------------------------------------------------------------------------
+// Unload Handler
+//------------------------------------------------------------------------------
+extern "C" DllExport int ufusr_ask_unload()
+{
+	return (int)NXOpen::Session::LibraryUnloadOptionImmediately;
+}
+
+
